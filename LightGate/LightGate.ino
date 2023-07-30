02e5c7f44d7716ab38604403053e59e081b84560
Faysal <82079632+FJay81@users.noreply.github.com>
2023-07-30 18:24:05 +0100

    Update LightGate.ino

--- LightGate/LightGate.ino
+++ LightGate/LightGate.ino
@@ -1,7 +1,7 @@
 byte LED = 8; //Set an output in this case a LED, can also be a electromagnet in a coil gun or a relay for a more powerful light such as incandesant bulb
 byte Sensor = A0; //analog input from the LDR
 byte Gate = 9; //Part of the light gate
-int thresh_hold = 250; //thresh hold directly affects the sensitivity, higher value is less sensitive, i found 250 works in a dimly lit room with a blue 5v LED about 10mm away from ghe LDR 
+int thresh_hold = 250; //thresh hold directly affects the sensitivity, higher value is less sensitive, i found 250 works in a dimly lit room with a blue 5v LED about 10mm away from the LDR 
 
 void setup() {
   pinMode(LED, OUTPUT);
@@ -15,12 +15,14 @@ void loop() {
   int val = analogRead(Sensor);
   if (val <= thresh_hold)
   {
+    //add code for when light is not present or blocked
     digitalWrite(LED, 1);
     Serial.print(val);
     Serial.print("\n");
   }
   else if (val > thresh_hold)
   {
+    //add code for when light is present or not blocked
     digitalWrite(LED, 0);
     Serial.print(val);
     Serial.print("\n");
